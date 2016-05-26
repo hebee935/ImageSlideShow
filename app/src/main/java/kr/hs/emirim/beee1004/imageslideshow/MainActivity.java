81eed6824211fcaa8643248b9222eabc76b3692d
Eunbee Hong <hebee935@gmail.com>
2016-05-26 20:33:04 +0900

    3 Event Programming_time edit

--- app/src/main/java/kr/hs/emirim/beee1004/imageslideshow/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/beee1004/imageslideshow/MainActivity.java
@@ -4,16 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     ViewFlipper viewFlip;
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceSta te);
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         viewFlip = (ViewFlipper)findViewById(R.id.view_flip);
-        viewFlip.setFlipInterval(1000);
+        editText = (EditText)findViewById(R.id.edit_text);
         Button butStart = (Button)findViewById(R.id.but_start);
         Button butStop = (Button)findViewById(R.id.but_stop);
         butStart.setOnClickListener(this);
@@ -21,6 +23,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
      public void onClick(View view){
+         int time = Integer.parseInt(editText.getText().toString());
+         viewFlip.setFlipInterval((1000*time));
          switch(view.getId()) {
              case R.id.but_start:
                  viewFlip.startFlipping();
