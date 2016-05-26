516dec783fbeebbf9b8735d45c132f01da7be5c2
Eunbee Hong <hebee935@gmail.com>
2016-05-26 20:53:34 +0900

    #3

--- app/src/main/java/kr/hs/emirim/beee1004/imageslideshow/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/beee1004/imageslideshow/MainActivity.java
@@ -23,10 +23,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
      public void onClick(View view){
-         int time = Integer.parseInt(editText.getText().toString());
-         viewFlip.setFlipInterval((1000*time));
+         int time = (int)Double.parseDouble(editText.getText().toString())*1000;
          switch(view.getId()) {
              case R.id.but_start:
+                 viewFlip.setFlipInterval(time);
                  viewFlip.startFlipping();
                  break;
              case R.id.but_stop:
